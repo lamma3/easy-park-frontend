3ca58022dc75eb63941bac5b62773d599a9d5b1e
Michelle Li <42404448+ltcmichelli@users.noreply.github.com>
2020-04-25 18:11:33 +0800

    [Michelle] Modify router path

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import ParkingLotList from './component/ParkingLotList';
 import ParkingLotInfo from './component/ParkingLotInfo';
 
 function App() {
-  
+
   return (
     <div className="App">
-        <AppTopNavBar />
-        <div>
-          <Route path="/search"  component={ParkingLotList} />
-          <Route path="/infos/:id" component={ParkingLotInfo} />
-        </div>
+      <AppTopNavBar />
+      <div>
+        <Route exact path="/" component={ParkingLotList}></Route>
+        {/* <Route path="/search" component={ParkingLotList} /> */}
+        <Route path="/infos/:id" component={ParkingLotInfo} />
+      </div>
     </div>
   );
 }
