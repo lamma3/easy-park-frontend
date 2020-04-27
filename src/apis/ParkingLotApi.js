1fcd6888cc9333c94334fd475c12e7a26600f0c8
Marco Lam <lamma3@oocl.com>
2020-04-27 14:16:22 +0800

    [Marco] extract api url as constant

--- src/apis/ParkingLotApi.js
+++ src/apis/ParkingLotApi.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
+import { API_URL } from '../constant/constants';
 
 class ParkingLotApi {
 
     static getAllParkingLotList(distance, minHourRate, maxHourRate, hasElectricCar, rate){
-        const GET_URL ="https://5ea2968bb9f5ca00166c2d36.mockapi.io/parking-lots";
+        const GET_URL = `${API_URL}/parking-lots`;
         return axios.get(GET_URL);
 
     }
 
     static getParkingLotById(id){
-        const GET_BY_ID_URL ="https://5ea2968bb9f5ca00166c2d36.mockapi.io/parking-lots/"+id;
+        const GET_BY_ID_URL = `${API_URL}/parking-lots/${id}`;
         return axios.get(GET_BY_ID_URL);
 
     }
--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,3 +1,4 @@
+export const API_URL = process.env.REACT_APP_API_URL;
 export const DUMMY_PARKING_LOT_LIST = [{
     parkingLotId: 1,
     parkingLotName: "科學園3期停車場",
