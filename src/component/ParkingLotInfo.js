8bba57347a214dce67cd8498cd9651559a107067
Michelle Li <42404448+ltcmichelli@users.noreply.github.com>
2020-04-28 15:18:20 +0800

    [Michelle] fix console error

--- src/component/ParkingLotInfo.js
+++ src/component/ParkingLotInfo.js
@@ -64,21 +64,21 @@ class ParkingLotInfo extends Component {
             <div className='Info-content'>
                 <div className='Display-card'>
                     <Title level={3}>{this.state.list.name}</Title>
-                    <div class="Display-container">
-                        <div class="Info-title"><Text>Address: </Text></div>
-                        <div class="Info-item"><Text>{this.state.list.address}</Text></div>
+                    <div className="Display-container">
+                        <div className="Info-title"><Text>Address: </Text></div>
+                        <div className="Info-item"><Text>{this.state.list.address}</Text></div>
                         
-                        <div class="Info-title"><Text>Hourly Rate: </Text></div>
-                        <div class="Info-item"><Text>${this.state.list.hourRate}</Text></div>
+                        <div className="Info-title"><Text>Hourly Rate: </Text></div>
+                        <div className="Info-item"><Text>${this.state.list.hourRate}</Text></div>
                         
-                        <div class="Info-title"><Text>Remaining Space: </Text></div>
-                        <div class="Info-item">
+                        <div className="Info-title"><Text>Remaining Space: </Text></div>
+                        <div className="Info-item">
                             <Text><CarFilled /> {this.state.list.availableCapacity}</Text><br/>
                             <Text><ThunderboltFilled /> {this.state.list.availableChargeCapacity}</Text>
                         </div> 
                         
-                        <div class="Info-title"><Text>Rating: </Text></div>
-                        <div class="Info-item"><Text>{this.state.list.rating.toFixed(1)} / 5.0</Text></div> 
+                        <div className="Info-title"><Text>Rating: </Text></div>
+                        <div className="Info-item"><Text>{this.state.list.rating.toFixed(1)} / 5.0</Text></div> 
                     </div>
                     <div className='Info-button'>
                         <Space size='small'>
