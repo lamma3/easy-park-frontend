35659786ccffe3caf1c0bd764ec4ea6dc79dc2fe
Marco Lam <lamma3@oocl.com>
2020-04-28 20:47:40 +0800

    add content for empty result

--- src/component/BookingHistory.js
+++ src/component/BookingHistory.js
@@ -5,6 +5,7 @@ import { Typography, Button, Space } from 'antd';
 import ParkingLotApi from '../apis/ParkingLotApi';
 import Cookies from 'js-cookie';
 import Loading from './ui/Loading';
+import { List } from 'antd-mobile';
 
 const { Title, Text } = Typography;
 
@@ -46,38 +47,40 @@ class BookingHistory extends Component {
     render() {
         if (!this.state.isLoaded) {
             return <Loading />;
-        } else{
+        } else {
         return (
-            <div className='Booking-content'>
-            <div className= 'History-list-content'>
-            
-            {this.state.bookings.map( (booking) =>(
-
-            <div key={booking.id} className='Display-card' style={{marginBottom:"15px"}}>
-                <Title level={3}>Booking History</Title>
-                <div className="Display-container">
-                    <div className="Info-title"><Text>Booking Date: </Text></div>
-                    <div className="Info-item"><Text>{booking.utilTimestamp}</Text></div>
-                    <div className="Info-title"><Text>Booking ID: </Text></div>
-                    <div className="Info-item"><Text>{booking.id}</Text></div>
-                    <div className="Info-title"><Text>Name: </Text></div>
-                    <div className="Info-item"><Text>{booking.parkingLot.name}</Text></div>
-                    <div className="Info-title"><Text>Address: </Text></div>
-                    <div className="Info-item"><Text>{booking.parkingLot.address}</Text></div>
-                    <div className="Info-title"><Text>Booking Status: </Text></div>
-                    <div className="Info-item"><Text>{booking.status}</Text></div>
+                <div className='Booking-content'>
+                    <div className= 'History-list-content'>
+                    
+                    {this.state.bookings.length <= 0 ?
+                        <List.Item>No booking history</List.Item> :
+                        this.state.bookings.map( (booking) =>(
+                            <div key={booking.id} className='Display-card' style={{marginBottom:"15px"}}>
+                                <Title level={3}>Booking History</Title>
+                                <div className="Display-container">
+                                    <div className="Info-title"><Text>Booking Date: </Text></div>
+                                    <div className="Info-item"><Text>{booking.utilTimestamp}</Text></div>
+                                    <div className="Info-title"><Text>Booking ID: </Text></div>
+                                    <div className="Info-item"><Text>{booking.id}</Text></div>
+                                    <div className="Info-title"><Text>Name: </Text></div>
+                                    <div className="Info-item"><Text>{booking.parkingLot.name}</Text></div>
+                                    <div className="Info-title"><Text>Address: </Text></div>
+                                    <div className="Info-item"><Text>{booking.parkingLot.address}</Text></div>
+                                    <div className="Info-title"><Text>Booking Status: </Text></div>
+                                    <div className="Info-item"><Text>{booking.status}</Text></div>
+                                </div>
+                                <div className='Info-button'>
+                                    <Space size='small'>
+                                        <Button type="primary" onClick={this.goBack}>Back</Button>
+                                        <Button onClick={() => this.goRatingPage(booking.parkingLot.id)}> Rate</Button>
+                                    </Space>
+                                </div>
+                            </div>    
+                        ))
+                    }
+                    </div>         
                 </div>
-                <div className='Info-button'>
-                    <Space size='small'>
-                        <Button type="primary" onClick={this.goBack}>Back</Button>
-                        <Button onClick={() => this.goRatingPage(booking.parkingLot.id)}> Rate</Button>
-                    </Space>
-                </div>
-            </div>    
-            ))}
-            </div>         
-            </div>
-        )
+            )
         }
     }
 }
