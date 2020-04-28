6d8a8496d8142e8d67934156be7fa0363d2b55a3
wongpa1 <61858077+wongpa1@users.noreply.github.com>
2020-04-28 22:37:01 +0800

    [Paul] Fixed the Booking data problem

--- src/component/BookingHistory.js
+++ src/component/BookingHistory.js
@@ -28,16 +28,10 @@ class BookingHistory extends Component {
     }
 
     timeStamp2String (time){
-                var datetime = new Date();
-                 datetime.setTime(time);
-                 var year = datetime.getFullYear();
-                 var month = datetime.getMonth() + 1;
-                 var date = datetime.getDate();
-                 var hour = datetime.getHours();
-                 var minute = datetime.getMinutes();
-
-                 return year + "-" + month + "-" + date+" "+hour+":"+minute;
-        };
+        var datetime = new Date(time);
+        return datatime.toString();
+                
+    }
      
 
     goRatingPage(id) {
